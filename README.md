b310a8b1a1660b66ae2cf772a487782383ca3c60
Jonathan Bieler <jonathan.bieler@alumni.epfl.ch>
2019-03-14 11:39:21 +0100

    added tests and licence

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # GtkExtensions
 
-Some extensions to [Gtk.jl](https://github.com/JuliaGraphics/Gtk.jl).
+[![Build Status](https://travis-ci.org/jonathanBieler/GtkExtensions.jl.svg?branch=master)](https://travis-ci.org/jonathanBieler/GtkExtensions.jl)
+
+Some extensions for [Gtk.jl](https://github.com/JuliaGraphics/Gtk.jl).
---
+++ test/runtests.jl
@@ -0,0 +1,19 @@
+using Test
+using GtkExtensions, Gtk
+
+@testset "TextIter" begin
+
+    import Gtk: GtkTextIter, mutable
+
+    w = GtkWindow()
+    b = GtkTextBuffer()
+    b.text[String] = "test"
+    v = GtkTextView(b)
+
+    push!(w,v)
+    showall(w)
+
+    it = GtkTextIter(b,1)
+    text_buffer_place_cursor(b,it)
+
+end
